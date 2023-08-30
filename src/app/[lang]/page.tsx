e9bef7af6db1d0c641215f40e66274061f627c2f
Johannes Naylor <jonaylor89@gmail.com>
2023-08-30 22:41:49 +0200

    no dark mode

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -9,7 +9,7 @@ export default async function Home({ lang }: { lang: Locale }) {
   return (
     <main className="flex min-h-screen bg-[#38B6FF] flex-col items-center justify-center p-12">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="md:hidden fixed bottom-0 left-0 flex h-40 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="md:hidden fixed bottom-0 left-0 flex h-40 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
               className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://app.tapped.ai"
