c09f880cae42501a04df3b8450d6e011eb968e46
Johannes Naylor <jonaylor89@gmail.com>
2023-08-30 22:58:06 +0200

    ignore logo

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,14 +28,16 @@ export function middleware(request: NextRequest) {
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  if (
+    [
+      '/manifest.json',
+      '/favicon.ico',
+      '/images/tapped_logo.png',
+      '/images/icon_1024.png',
+      // Your other files in `public`
+    ].includes(pathname)
+  )
+    return
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
