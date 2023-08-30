4377972c0a3e487cb1406e88939f17e35df1a3f1
Johannes Naylor <jonaylor89@gmail.com>
2023-08-30 13:05:50 +0200

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen bg-[#38B6FF] flex-col items-center justify-center p-12">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="md:hidden fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="md:hidden fixed bottom-0 left-0 flex h-40 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
               className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://app.tapped.ai"
@@ -71,9 +71,9 @@ export default function Home() {
           priority
         />
       </div>
-      <div className="text-center lg:max-w-5xl lg:w-full">
+      <div className="text-center lg:max-w-5xl lg:w-full pb-12">
 
-        <div className="text-5xl font-extrabold p-2 text-white">
+        <div className="text-5xl font-extrabold m-2 text-white">
           {idea}
         </div>
         <div className="p-6"></div>
