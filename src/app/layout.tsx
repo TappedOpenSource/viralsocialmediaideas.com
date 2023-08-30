afcf0d7d7eaeecba2c73092a00387be110c8ae53
Johannes Naylor <jonaylor89@gmail.com>
2023-08-30 12:56:06 +0200

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,34 @@ const arimo = Arimo({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Viral Social Media Ideas',
   description: 'Generate viral social media idea with Ai for musicians',
+  applicationName: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  authors: [{ name: 'Johannes', url: 'https://jonaylor.xyz' }],
+  creator: 'Johannes Naylor',
+  publisher: 'Johannes Naylor',
+  metadataBase: new URL('https://viralsocialmediaideas.com/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  openGraph: {
+    title: 'Viral Social Media Ideas',
+    description: 'Generate viral social media idea with Ai for musicians',
+    url: 'https://viralsocialmediaideas.com/',
+    siteName: 'Viral Social Media Ideas',
+    images: [
+      {
+        url: 'https://viralsocialmediaideas/og.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
@@ -17,6 +45,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:title"
+        content="Viral Social Media Ideas"
+      />
+      <meta
+        name="twitter:description"
+        content="Generate viral social media idea with Ai for musicians"
+      />
+      <meta property="twitter:image" content="https://viralsocialmediaideas.com/og.png"></meta>
       <body className={arimo.className}>
         {children}
         <Analytics />
