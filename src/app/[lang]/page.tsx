26fc877364a1955a440fbea1adc114c0383b5e93
Johannes Naylor <jonaylor89@gmail.com>
2023-10-12 19:38:09 -0500

    nav

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,52 +1,30 @@
+'use client';
+
 import Image from 'next/image';
 import { getDictionary } from '@/dictionaries';
 import { Locale } from '@/i18n-config';
 import RandomIdea from './components/RandomIdea';
+import Nav from '@/components/Nav';
 
 const Home = async ({ params }: { params: { lang: Locale }}) => {
   const dict = await getDictionary(params.lang); // en
 
   return (
-    <main className="flex min-h-screen bg-[#38B6FF] flex-col items-center justify-center p-12">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="md:hidden fixed bottom-0 left-0 flex h-40 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://app.tapped.ai"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              By{' '}
-            <Image
-              src="/images/tapped_logo.png"
-              alt="Tapped AI Logo"
-              width={48}
-              height={14}
-              priority
-            />
-          </a>
-          <a
-            className="md:hidden underline text-black flex place-items-center gap-2 p-8 lg:pointer-events-auto"
-            href="https://app.tapped.ai/download"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              Download the app
-          </a>
+    <div className='h-screen'>
+      <Nav />
+      <main className="flex bg-[#38B6FF] flex-col items-center justify-center p-12">
+        <div className="hidden md:block py-12">
+          <Image
+            src="/images/icon_1024.png"
+            alt="Tapped AI Logo"
+            width={200}
+            height={200}
+            priority
+          />
         </div>
-      </div>
-
-      <div className="hidden md:block py-12">
-        <Image
-          src="/images/icon_1024.png"
-          alt="Tapped AI Logo"
-          width={200}
-          height={200}
-          priority
-        />
-      </div>
-      <RandomIdea ideas={dict.postIdeas} />
-    </main>
+        <RandomIdea ideas={dict.postIdeas} />
+      </main>
+    </div>
   );
 };
 
---
+++ src/components/Nav.tsx
@@ -0,0 +1,76 @@
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+const Nav = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <>
+      <nav className='flex items-center flex-wrap bg-transparent p-3 '>
+
+        <Link
+          href="https://tapped.ai"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='inline-flex items-center p-2 mr-4 '
+        >
+          <span className='text-xl text-white font-bold uppercase tracking-wide'>
+              TAPPED AI
+          </span>
+        </Link>
+        <button
+          className=' inline-flex p-3 hover:bg-blue-500 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+          onClick={handleClick}
+        >
+
+        </button>
+        {/* Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? '' : 'hidden'
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link
+              href='https://tapped.ai'
+              target="_blank"
+              rel="noopener noreferrer"
+              className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white '
+            >
+                Home
+            </Link>
+            <Link
+              href='https://getmusicmarketing.com'
+              target="_blank"
+              rel="noopener noreferrer"
+              className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white'
+            >
+                Marketing
+            </Link>
+            <Link
+              href='https://viralsocialmediaideas.com'
+              target="_blank"
+              rel="noopener noreferrer"
+              className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white'
+            >
+                Social Media
+            </Link>
+            <Link
+              href='mailto://support@tapped.ai'
+              className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white'
+            >
+                Contact Us
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
